c251f61781923250d6fd0f10f5b661db7f98e76a
Liangsheng <liangshe@princeton.edu>
2015-04-14 17:12:25 -0400

    Add constants header file

---
+++ constants.h
@@ -0,0 +1,24 @@
+//
+// Created by Liangsheng Zhang on 4/14/15.
+//
+
+#ifndef MBL_V1_CONSTANTS_H
+#define MBL_V1_CONSTANTS_H
+
+#include <complex>
+
+using namespace std;
+
+/*
+ * Constants used throughout files
+ */
+
+const double Pi = 3.1415926535897932;
+
+const complex<double> Complex_zero = complex<double>(0,0);
+
+const complex<double> Complex_one = complex<double>(1,0);
+
+const complex<double> Complex_I = complex<double>(0,1);
+
+#endif //MBL_V1_CONSTANTS_H
