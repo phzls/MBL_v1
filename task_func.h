87d11c00e78ecfacf655553fd609e58a04579567
Liangsheng <liangshe@princeton.edu>
2015-04-15 18:36:22 -0400

    Add header file for task functions

---
+++ task_func.h
@@ -0,0 +1,19 @@
+//
+// Created by Liangsheng Zhang on 4/15/15.
+//
+
+/*
+ * This file includes all the tasks that can be called from the main function.
+ */
+
+#ifndef MBL_V1_TASK_FUNC_H
+#define MBL_V1_TASK_FUNC_H
+
+#include "parameters.h"
+
+/*
+ * Study transition properties of a disorder model
+ */
+void disorder_transition(const AllPara&);
+
+#endif //MBL_V1_TASK_FUNC_H
