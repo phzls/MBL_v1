b2a039e5633ad6caa282e40c688091a6b431ffb5
Liangsheng <liangshe@princeton.edu>
2015-04-16 11:46:00 -0400

    Add functions in matrix_algebra which deal with vectors

--- matrix_algebra.cpp
+++ matrix_algebra.cpp
@@ -22,7 +22,7 @@ void Matrix_Add(const MatrixXd& A, const MatrixXd& B, MatrixXcd& C){
     MatrixXd temp;
     temp = A + B;
 
-    C = MatrixXcd::Zero(C.rows(), C.cols());
+    C = MatrixXcd::Zero(temp.rows(), temp.cols());
 
     for (int i=0; i<C.cols(); i++){
         for (int j=0; j<C.rows(); j++){
@@ -73,7 +73,7 @@ void Matrix_Sub(const MatrixXd& A, const MatrixXd& B, MatrixXcd& C){
     MatrixXd temp;
     temp = A - B;
 
-    C = MatrixXcd::Zero(C.rows(), C.cols());
+    C = MatrixXcd::Zero(temp.rows(), temp.cols());
 
     for (int i=0; i<C.cols(); i++){
         for (int j=0; j<C.rows(); j++){
@@ -125,7 +125,7 @@ void Matrix_Mul(const MatrixXd& A, const MatrixXd& B, MatrixXcd& C){
     MatrixXd temp;
     temp = A * B;
 
-    C = MatrixXcd::Zero(C.rows(), C.cols());
+    C = MatrixXcd::Zero(temp.rows(), temp.cols());
 
     for (int i=0; i<C.cols(); i++){
         for (int j=0; j<C.rows(); j++){
@@ -166,4 +166,192 @@ void Matrix_Mul(const MatrixXcd& A, const MatrixXcd& B, MatrixXcd& C){
     C = A * B;
 }
 
+void Matrix_Add(const MatrixXd& A, const MatrixXd& B, VectorXd& C){
+    Matrix_Add_Sub_Check(A,B);
+    Vector_Check(A);
+    Vector_Check(B);
 
+    C = A + B;
+}
+
+void Matrix_Add(const MatrixXd& A, const MatrixXd& B, VectorXcd& C){
+    Matrix_Add_Sub_Check(A,B);
+    Vector_Check(A);
+    Vector_Check(B);
+
+    MatrixXd temp;
+    temp = A + B;
+
+    C = MatrixXcd::Zero(C.rows(), C.cols());
+
+    for (int i=0; i<C.cols(); i++){
+        for (int j=0; j<C.rows(); j++){
+            C(j,i) = complex<double>(temp(j,i),0);
+        }
+    }
+}
+
+void Matrix_Add(const MatrixXd& A, const MatrixXcd& B, VectorXcd& C){
+    Matrix_Add_Sub_Check(A,B);
+    Vector_Check(A);
+    Vector_Check(B);
+
+    MatrixXcd temp(A.cols(),A.rows());
+
+    for (int i=0; i<temp.cols(); i++){
+        for (int j=0; j<temp.rows(); j++){
+            temp(j,i) = complex<double>(A(j,i),0);
+        }
+    }
+
+    C = temp + B;
+}
+
+void Matrix_Add(const MatrixXcd& A, const MatrixXd& B, VectorXcd& C){
+    Matrix_Add_Sub_Check(A,B);
+    Vector_Check(A);
+    Vector_Check(B);
+
+    MatrixXcd temp(B.cols(),B.rows());
+
+    for (int i=0; i<temp.cols(); i++){
+        for (int j=0; j<temp.rows(); j++){
+            temp(j,i) = complex<double>(B(j,i),0);
+        }
+    }
+
+    C = A + temp;
+}
+
+void Matrix_Add(const MatrixXcd& A, const MatrixXcd& B, VectorXcd& C){
+    Matrix_Add_Sub_Check(A,B);
+    Vector_Check(A);
+    Vector_Check(B);
+
+    C = A + B;
+}
+
+void Matrix_Sub(const MatrixXd& A, const MatrixXd& B, VectorXd& C){
+    Matrix_Add_Sub_Check(A,B);
+    Vector_Check(A);
+    Vector_Check(B);
+
+    C = A - B;
+}
+
+void Matrix_Sub(const MatrixXd& A, const MatrixXd& B, VectorXcd& C){
+    Matrix_Add_Sub_Check(A,B);
+    Vector_Check(A);
+    Vector_Check(B);
+
+    MatrixXd temp;
+    temp = A - B;
+
+    C = MatrixXcd::Zero(C.rows(), C.cols());
+
+    for (int i=0; i<C.cols(); i++){
+        for (int j=0; j<C.rows(); j++){
+            C(j,i) = complex<double>(temp(j,i),0);
+        }
+    }
+}
+
+void Matrix_Sub(const MatrixXd& A, const MatrixXcd& B, VectorXcd& C){
+    Matrix_Add_Sub_Check(A,B);
+    Vector_Check(A);
+    Vector_Check(B);
+
+    MatrixXcd temp(A.cols(),A.rows());
+
+    for (int i=0; i<temp.cols(); i++){
+        for (int j=0; j<temp.rows(); j++){
+            temp(j,i) = complex<double>(A(j,i),0);
+        }
+    }
+
+    C = temp - B;
+}
+
+void Matrix_Sub(const MatrixXcd& A, const MatrixXd& B, VectorXcd& C){
+    Matrix_Add_Sub_Check(A,B);
+    Vector_Check(A);
+    Vector_Check(B);
+
+    MatrixXcd temp(B.cols(),B.rows());
+
+    for (int i=0; i<temp.cols(); i++){
+        for (int j=0; j<temp.rows(); j++){
+            temp(j,i) = complex<double>(B(j,i),0);
+        }
+    }
+
+    C = A - temp;
+}
+
+void Matrix_Sub(const MatrixXcd& A, const MatrixXcd& B, VectorXcd& C){
+    Matrix_Add_Sub_Check(A,B);
+    Vector_Check(A);
+    Vector_Check(B);
+
+    C = A - B;
+}
+
+void Matrix_Mul(const MatrixXd& A, const MatrixXd& B, VectorXd& C){
+    Matrix_Mul_Check(A,B);
+    Mul_Vector_Check(A,B);
+
+    C = A * B;
+}
+
+void Matrix_Mul(const MatrixXd& A, const MatrixXd& B, VectorXcd& C){
+    Matrix_Mul_Check(A,B);
+    Mul_Vector_Check(A,B);
+
+    MatrixXd temp;
+    temp = A * B;
+
+    C = MatrixXcd::Zero(C.rows(), C.cols());
+
+    for (int i=0; i<C.cols(); i++){
+        for (int j=0; j<C.rows(); j++){
+            C(j,i) = complex<double>(temp(j,i),0);
+        }
+    }
+}
+
+void Matrix_Mul(const MatrixXd& A, const MatrixXcd& B, VectorXcd& C){
+    Matrix_Mul_Check(A,B);
+    Mul_Vector_Check(A,B);
+
+    MatrixXcd temp(A.cols(),A.rows());
+
+    for (int i=0; i<temp.cols(); i++){
+        for (int j=0; j<temp.rows(); j++){
+            temp(j,i) = complex<double>(A(j,i),0);
+        }
+    }
+
+    C = temp * B;
+}
+
+void Matrix_Mul(const MatrixXcd& A, const MatrixXd& B, VectorXcd& C){
+    Matrix_Mul_Check(A,B);
+    Mul_Vector_Check(A,B);
+
+    MatrixXcd temp(B.cols(),B.rows());
+
+    for (int i=0; i<temp.cols(); i++){
+        for (int j=0; j<temp.rows(); j++){
+            temp(j,i) = complex<double>(B(j,i),0);
+        }
+    }
+
+    C = A * temp;
+}
+
+void Matrix_Mul(const MatrixXcd& A, const MatrixXcd& B, VectorXcd& C){
+    Matrix_Mul_Check(A,B);
+    Mul_Vector_Check(A,B);
+
+    C = A * B;
+}
--- matrix_algebra.h
+++ matrix_algebra.h
@@ -21,17 +21,35 @@ void Matrix_Add(const MatrixXd&, const MatrixXcd&, MatrixXcd&);
 void Matrix_Add(const MatrixXcd&, const MatrixXd&, MatrixXcd&);
 void Matrix_Add(const MatrixXcd&, const MatrixXcd&, MatrixXcd&);
 
+void Matrix_Add(const MatrixXd&, const MatrixXd&, VectorXd&);
+void Matrix_Add(const MatrixXd&, const MatrixXd&, VectorXcd&);
+void Matrix_Add(const MatrixXd&, const MatrixXcd&, VectorXcd&);
+void Matrix_Add(const MatrixXcd&, const MatrixXd&, VectorXcd&);
+void Matrix_Add(const MatrixXcd&, const MatrixXcd&, VectorXcd&);
+
 void Matrix_Sub(const MatrixXd&, const MatrixXd&, MatrixXd&);
 void Matrix_Sub(const MatrixXd&, const MatrixXd&, MatrixXcd&);
 void Matrix_Sub(const MatrixXd&, const MatrixXcd&, MatrixXcd&);
 void Matrix_Sub(const MatrixXcd&, const MatrixXd&, MatrixXcd&);
 void Matrix_Sub(const MatrixXcd&, const MatrixXcd&, MatrixXcd&);
 
+void Matrix_Sub(const MatrixXd&, const MatrixXd&, VectorXd&);
+void Matrix_Sub(const MatrixXd&, const MatrixXd&, VectorXcd&);
+void Matrix_Sub(const MatrixXd&, const MatrixXcd&, VectorXcd&);
+void Matrix_Sub(const MatrixXcd&, const MatrixXd&, VectorXcd&);
+void Matrix_Sub(const MatrixXcd&, const MatrixXcd&, VectorXcd&);
+
 void Matrix_Mul(const MatrixXd&, const MatrixXd&, MatrixXd&);
 void Matrix_Mul(const MatrixXd&, const MatrixXd&, MatrixXcd&);
 void Matrix_Mul(const MatrixXd&, const MatrixXcd&, MatrixXcd&);
 void Matrix_Mul(const MatrixXcd&, const MatrixXd&, MatrixXcd&);
 void Matrix_Mul(const MatrixXcd&, const MatrixXcd&, MatrixXcd&);
+
+void Matrix_Mul(const MatrixXd&, const MatrixXd&, VectorXd&);
+void Matrix_Mul(const MatrixXd&, const MatrixXd&, VectorXcd&);
+void Matrix_Mul(const MatrixXd&, const MatrixXcd&, VectorXcd&);
+void Matrix_Mul(const MatrixXcd&, const MatrixXd&, VectorXcd&);
+void Matrix_Mul(const MatrixXcd&, const MatrixXcd&, VectorXcd&);
 
 template <class T1, class T2>
 void Matrix_Mul_Check(const MatrixBase<T1>& A, const MatrixBase<T2>& B){
@@ -58,6 +76,28 @@ void Matrix_Add_Sub_Check(const MatrixBase<T1>& A, const MatrixBase<T2>& B){
         cout << "Row dimension does not match for matrix multiplication." << endl;
         cout << "First matrix row num: " << A.rows() << endl;
         cout << "Second matrix row num: " << B.rows() << endl;
+        abort();
+    }
+}
+
+template <class T>
+void Vector_Check(const MatrixBase<T>& A){
+    // Check if it is a vector
+    if (A.cols() != 1 && A.rows() != 1){
+        cout << "Matrix is not of vector shape." << endl;
+        cout << "Row num: " << A.rows() << endl;
+        cout << "Col num: " << A.cols() << endl;
+        abort();
+    }
+}
+
+template <class T1, class T2>
+void Mul_Vector_Check(const MatrixBase<T1>& A,  const MatrixBase<T2>& B){
+    // Check if the multiplication results in a vector
+    if (A.rows() != 1 && B.cols() != 1){
+        cout << "Multiplication does not give a vector." << endl;
+        cout << "First matrix row num: " << A.rows() << endl;
+        cout << "Second matrix col num: " << B.cols() << endl;
         abort();
     }
 }
