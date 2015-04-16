f4614c5b8d763f9d47c8aec683f4c91ad5ac6ebc
Liangsheng Zhang <liangshe@feynman.princeton.edu>
2015-04-16 18:01:24 -0400

    Update main

--- main.cpp
+++ main.cpp
@@ -29,9 +29,9 @@ int main() {
     parameters.generic.task = "Disorder Transition";
     parameters.generic.model = "Ising Random Simp Shift Real Flo";
 
-    parameters.generic.size = 8; // System size
-    parameters.generic.num_realizations = 100; // Number of realizations
-    parameters.generic.threads_N = 8; // Number of threads in openmp
+    parameters.generic.size = 13; // System size
+    parameters.generic.num_realizations = 12; // Number of realizations
+    parameters.generic.threads_N = 6; // Number of threads in openmp
     parameters.generic.debug = false; // Whether output debug information
     parameters.generic.iso_keep = true; // Whether isolated part is kept
     parameters.generic.version = 1; // Version of the output
@@ -40,9 +40,9 @@ int main() {
     parameters.output.width = 30; // Width for spacing in output files
     parameters.output.filename_output = true; // Whether print out file names
 
-    parameters.floquet.J_N = 30; // Number of points of coupling strength
-    parameters.floquet.J_min = 0.1; // Minimum J
-    parameters.floquet.J_max = 0.9; // Maximum J
+    parameters.floquet.J_N = 8; // Number of points of coupling strength
+    parameters.floquet.J_min = 0.5; // Minimum J
+    parameters.floquet.J_max = 0.7; // Maximum J
     parameters.floquet.tau = 0.8; // Time step size
     parameters.floquet.J = 0.6;
 
@@ -125,7 +125,7 @@ int main() {
     parameters.transition.flo_transition_compute["ZZ Correlation Square"] = true; // End-to-end sigma_z X sigma_z
     // correlation square
 
-    parameters.transition.flo_transition_compute["Entropy Variance"] = true; // Entropy variance for all eigenstates
+    parameters.transition.flo_transition_compute["Entropy Variance"] = false; // Entropy variance for all eigenstates
 
     parameters.transition.flo_transition_compute["ZZ Time Correlation"] = true; // End-to-end sigma_z X sigma_z
     // time correlation
