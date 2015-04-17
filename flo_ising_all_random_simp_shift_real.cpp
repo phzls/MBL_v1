4b1b323013338341a85cb1b4fc6062d5fa0dafec
Liangsheng <liangshe@princeton.edu>
2015-04-17 16:33:05 -0400

    Implement flo_ising_all_random_simp_shift_real

---
+++ flo_ising_all_random_simp_shift_real.cpp
@@ -0,0 +1,216 @@
+//
+// Created by Liangsheng Zhang on 4/17/15.
+//
+
+/*
+ * Implementation of FloEvolIsingAllRandomSimpReal class
+ */
+
+#include <complex>
+#include <iostream>
+#include "constants.h"
+#include "flo_evol_model.h"
+#include "screen_output.h"
+#include "randomc.h"
+
+using namespace std;
+
+extern CRandomMersenne RanGen_mersenne; // points in [0,1)
+
+/*
+ * Construct the representation string and abstract type of the class.
+ */
+void FloEvolIsingAllRandomSimpShiftReal::Repr_Init_(){
+    repr_ << "Ising_All_Random_Simp_Shift_Real_Floquet_L=" << size_ << ",W=" << W_;
+    type_ = "Ising_All_Random_Simp_Shift_Real_Floquet";
+}
+
+/*
+ * Initialize random numbers
+ */
+void FloEvolIsingAllRandomSimpShiftReal::Evol_Para_Init() {
+    random_h_.resize(size_);
+    random_J_.resize(size_-1);
+    random_g_.resize(size_);
+
+    // Random Fields
+    for (int i=0; i<size_;i++){
+        double u = RanGen_mersenne.Random();
+        random_h_[i] = 1 + W_ * (2*u-1);
+
+        if (i<size_-1){
+            u = RanGen_mersenne.Random();
+            random_J_[i] = sqrt(1-W_) * (1-W_ ) + W_ * sqrt(1-W_) * u;
+        }
+
+        u = RanGen_mersenne.Random();
+        random_g_[i] = 1 + W_ * (2*u-1);
+    }
+
+    if (debug_){
+        cout << "Random longitude field:" << endl;
+        for (int i=0; i<size_;i++) cout << random_h_[i] << endl;
+        cout << endl;
+
+        cout << "Random NN interaction:" << endl;
+        for (int i=0; i<size_-1;i++) cout << random_J_[i] << endl;
+        cout << endl;
+
+        cout << "Random transverse field:" << endl;
+        for (int i=0; i<size_;i++) cout << random_g_[i] << endl;
+        cout << endl;
+    }
+}
+
+/*
+ * Construct the time evolution matrix
+ */
+void FloEvolIsingAllRandomSimpShiftReal::Evol_Construct() {
+
+    if (!constructed_){
+        evol_op_ = MatrixXcd::Zero(dim_, dim_);
+        evol_op_real_ = MatrixXd::Zero(dim_, dim_);
+        evol_op_imag_ = MatrixXd::Zero(dim_, dim_);
+        constructed_ = true;
+    }
+    else{
+        cout << "Evolution operator has been constructed." << endl;
+        abort();
+    }
+
+    MatrixXcd evol_half_x = MatrixXcd::Zero(dim_,dim_);
+    MatrixXcd evol_z = MatrixXcd::Zero(dim_,dim_);
+
+    Evol_X_Construct_(evol_half_x);
+    Evol_Z_Construct_(evol_z);
+
+    evol_op_ = evol_half_x * evol_z * evol_half_x;
+
+    for (int i=0; i<evol_op_.cols();i++){
+        for (int j=0; j<evol_op_.rows();j++){
+            evol_op_real_(j,i) = real(evol_op_(j,i));
+            evol_op_imag_(j,i) = imag(evol_op_(j,i));
+        }
+    }
+
+    // Check if the matrix is Hermitian
+    for (int i=0; i<evol_op_real_.cols(); i++){
+        for (int j=i+1; j< evol_op_real_.rows();j++){
+            if (abs(evol_op_real_(j,i) - evol_op_real_(i,j)) > 5.0e-6){
+                cout << "Real part is not Hermitian at row " << j <<" and col " << i << endl;
+                cout << "(i,j): " << evol_op_real_(i,j) << endl;
+                cout << "(j,i): " << evol_op_real_(j,i) << endl;
+                abort();
+            }
+        }
+    }
+
+    // Check if the matrix is Hermitian
+    for (int i=0; i<evol_op_imag_.cols(); i++){
+        for (int j=i+1; j< evol_op_imag_.rows();j++){
+            if (abs(evol_op_imag_(j,i) - evol_op_imag_(i,j)) > 5.0e-6){
+                cout << "Imaginary part is not Hermitian at row " << j <<" and col " << i << endl;
+                cout << "(i,j): " << evol_op_imag_(i,j) << endl;
+                cout << "(j,i): " << evol_op_imag_(j,i) << endl;
+                abort();
+            }
+        }
+    }
+
+    if (debug_){
+        cout << "Sqrt X part of evolution matrix:" << endl;
+        matrix_write(evol_half_x);
+        cout << endl;
+
+        cout << "Z part of evolution matrix:" << endl;
+        matrix_write(evol_z);
+        cout << endl;
+
+        cout << "Evolution matrix:" << endl;
+        matrix_write(evol_op_);
+        cout << endl;
+
+        cout << "Real part of evolution matrix:" << endl;
+        matrix_write(evol_op_real_);
+        cout << endl;
+
+        cout << "Imaginary part of evolution matrix:" << endl;
+        matrix_write(evol_op_imag_);
+        cout << endl;
+    }
+}
+
+/*
+ * Construct X part of the evolution matrix
+ */
+void FloEvolIsingAllRandomSimpShiftReal::Evol_X_Construct_(MatrixXcd & evol_half_x) {
+    for (int i=0; i<dim_; i++){
+        int flip_spin = 1;
+        for (int j=0;j<size_;j++){
+            int pos = i ^ flip_spin;
+            evol_half_x(pos,i) += random_g_[j];
+            flip_spin = flip_spin << 1;
+        }
+    }
+
+    // Check if the matrix is Hermitian
+    for (int i=0; i<evol_half_x.cols(); i++){
+        for (int j=i+1; j< evol_half_x.rows();j++){
+            if (abs(evol_half_x(j,i) - evol_half_x(i,j)) > 1.0e-10){
+                cout << "x part is not Hermitian at row " << j <<" and col " << i << endl;
+                cout << "(i,j): " << evol_half_x(i,j) << endl;
+                cout << "(j,i): " << evol_half_x(j,i) << endl;
+                abort();
+            }
+        }
+    }
+
+    SelfAdjointEigenSolver<MatrixXcd> x_eigen;
+
+    x_eigen.compute(evol_half_x);
+
+    for (int i=0; i<dim_; i++){
+        for (int j=0; j< dim_; j++){
+            if (i==j){
+                evol_half_x(i,j) = exp( - Complex_I * x_eigen.eigenvalues()[i] * complex<double>(0.5,0) );
+            }
+            else{
+                evol_half_x(i,j) = complex<double>(0,0);
+            }
+        }
+    }
+
+    evol_half_x = x_eigen.eigenvectors() * evol_half_x * x_eigen.eigenvectors().adjoint();
+}
+
+/*
+ *  Construct z part of the evolution matrix
+ */
+void FloEvolIsingAllRandomSimpShiftReal::Evol_Z_Construct_(MatrixXcd & evol_z) {
+    for (int i=0; i<dim_;i++){
+
+        int state = i;
+        int spin = 0, prev_spin = 0;
+        double value = 0; // value which will be exponentiated
+
+        for (int j=0; j<size_;j++){
+            if (j>0) prev_spin = spin;
+            spin = 2*(state & 1) - 1;
+            state = state >> 1;
+
+            value += random_h_[j] * spin;
+
+            if (j>0) value += spin * prev_spin * random_J_[j-1];
+        }
+
+        evol_z(i,i) = exp( - Complex_I * complex<double>(value,0) );
+    }
+}
+
+
+
+
+
+
+
+
