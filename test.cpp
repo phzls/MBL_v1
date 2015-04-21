9542a9659cdc43bf582816ce4d0215853ea41120
Liangsheng <liangshe@princeton.edu>
2015-04-21 18:40:53 -0400

    Update test file

--- test.cpp
+++ test.cpp
@@ -20,18 +20,24 @@ CRandomMersenne RanGen_mersenne(time(NULL));
 
 TasksModels tasks_models; // Record all the tasks and methods
 
+int EvolOP::model_num = 0;
+
 int main(){
 
     clock_t time_begin = clock();
     EvolOP* floquet;
 
-    floquet = new FloEvolIsingRandomSimpShiftReal(11, 0.6, false);
+    floquet = new FloEvolIsingQuasiSimpShiftReal(4, 0.6, false);
 
     floquet -> Evol_Para_Init();
     floquet -> Evol_Construct();
+    clock_t time_end = clock();
+    cout << "Construction time: " << double(time_end - time_begin) / CLOCKS_PER_SEC << "s" << endl;
+
+    time_begin = clock();
     floquet -> Evol_Diag();
 
-    vector<MatrixXcd> evec;
+    vector<MatrixXd> evec;
     vector<VectorXcd> eval;
 
     floquet -> Evec(evec);
@@ -39,11 +45,19 @@ int main(){
 
     floquet -> Eigen_Erase();
 
-    clock_t time_end = clock();
+    time_end = clock();
 
     cout << "Floquet time: " << double(time_end - time_begin) / CLOCKS_PER_SEC << "s" << endl;
 
 /*    cout << "Eigenvalues:" << endl;
+    complex_matrix_write(eval[0]);
+    cout << endl;
+
+    cout << "Eigenvectors:" << endl;
+    real_matrix_write(evec[0]);
+    cout << endl;
+
+    cout << "Eigenvalues:" << endl;
     for (int i=0; i<eval.size();i++){
         cout << "Sec " << i << endl;
         complex_matrix_write(eval[i]);
