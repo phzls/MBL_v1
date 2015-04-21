9aa5ccfc95df8d6fd94f80f3579fa3c61318bdff
Liangsheng <liangshe@princeton.edu>
2015-04-20 20:13:31 -0400

    Modify main_auto to correctly handle data file names

--- main_auto.cpp
+++ main_auto.cpp
@@ -19,10 +19,22 @@ TasksModels tasks_models; // Record all the tasks and methods
 
 int EvolOP::model_num = 0;
 
-int main() {
+int main(int argc, char* argv[]){
     AllPara parameters;
 
-    typedef void (*para)(AllPara&); // All parameters-reading functions
+
+    if (argc != 2){
+        cout << "The program takes one argument: count." << endl;
+        cout << "Number of argument: " << argc << endl;
+        abort();
+    }
+
+    string count = argv[1];
+
+
+    cout << "Count is " << count << endl;
+
+    typedef void (*para)(AllPara&, string); // All parameters-reading functions
 
     map<string, para> model_para_read_map;
     map<string, para> task_para_read_map;
@@ -34,19 +46,25 @@ int main() {
 
     task_para_read_map["Disorder_Transition"] = Disorder_Transition_Para;
 
+    cout << "Generic parameters:"<< endl;
     // Obtain generic parameters
-    Generic_Para(parameters);
+    Generic_Para(parameters, count);
 
+    cout << "Output parameters:" << endl;
     // Obtain output parameters
-    Output_Para(parameters);
+    Output_Para(parameters, count);
 
+    cout << "Model parameters:" << endl;
     // Obtain model parameters
-    model_para_read_map[parameters.generic.model](parameters);
+    model_para_read_map[parameters.generic.model](parameters,count);
 
+    cout << "Task parameters:" << endl;
     // Obtain task parameters
-    task_para_read_map[parameters.generic.task](parameters);
+    task_para_read_map[parameters.generic.task](parameters, count);
 
     Eigen::initParallel();
+
+    cout << "Start running." << endl;
 
     tasks_models.Task(parameters.generic.task)(parameters);
 
