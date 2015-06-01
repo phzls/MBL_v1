d7b32ae4ab9f74ba9693d87790df5a31435aedba
Liangsheng <liangshe@princeton.edu>
2015-06-01 11:40:39 -0400

    Implement init_obj for single_model_time_evolution using old code

---
+++ init_obj.cpp
@@ -0,0 +1,181 @@
+//
+// Created by Liangsheng Zhang on 6/1/15.
+//
+
+#include <iostream>
+#include <map>
+#include <sstream>
+#include "init_obj.h"
+
+using namespace std;
+
+InitInfo::InitInfo(const InitInfo& init_info){
+    size = init_info.size;
+    dim = init_info.dim;
+    norm_delta = init_info.norm_delta;
+    debug = init_info.debug;
+}
+
+init_func InitObj::Init_Func(const string& init_func_name) const {
+    map<string, init_func>::const_iterator it = init_func_map_.find(init_func_name);
+
+    if (it == init_func_map_.end()){
+        cout << "The requested init_func does not exist." << endl;
+        Print();
+        cout << "Requested function: " << init_func_name << endl;
+        abort();
+    }
+    else return it -> second;
+}
+
+init_func_C InitObj::Init_Func_C(const string& init_func_name) const {
+    map<string, init_func_C>::const_iterator it = init_func_C_map_.find(init_func_name);
+
+    if (it == init_func_C_map_.end()){
+        cout << "The requested init_func does not exist." << endl;
+        Print_C();
+        cout << "Requested function: " << init_func_name << endl;
+        abort();
+    }
+    else return it -> second;
+}
+
+void InitObj::Print() const {
+    map<string, init_func>::const_iterator it;
+
+    cout << "Initial State Construction Functions: "<< endl;
+    for (it = init_func_map_.begin(); it != init_func_map_.end(); it++){
+        cout << it -> first << endl;
+    }
+}
+
+void InitObj::Print_C() const {
+    map<string, init_func_C>::const_iterator it;
+
+    cout << "Initial State Construction Functions: "<< endl;
+    for (it = init_func_C_map_.begin(); it != init_func_C_map_.end(); it++){
+        cout << it -> first << endl;
+    }
+}
+
+void InitObj::map_init_(){
+    map<string, init_func>::const_iterator it;
+    map<string, init_func_C>::const_iterator it_C;
+
+    string name1 = "Random Product";
+    init_func func1 = random_product;
+    init_func_C func_C1 = random_product;
+
+    it = init_func_map_.find(name1);
+    if (it != init_func_map_.end()){
+        cout << "init_func " << name1 << " already exists." << endl;
+        abort();
+    }
+    init_func_map_[name1] = func1;
+
+    it_C = init_func_C_map_.find(name1);
+    if (it_C != init_func_C_map_.end()){
+        cout << "init_func " << name1 << " already exists." << endl;
+        abort();
+    }
+    init_func_C_map_[name1] = func_C1;
+
+    string name2 = "Product Random";
+    init_func func2 = product_random;
+
+    it = init_func_map_.find(name2);
+    if (it != init_func_map_.end()){
+        cout << "init_func " << name2 << " already exists." << endl;
+        abort();
+    }
+    init_func_map_[name2] = func2;
+
+    string name3 = "Random Pure";
+    init_func_C func_C3 = random_pure;
+
+    it_C = init_func_C_map_.find(name3);
+    if (it_C != init_func_C_map_.end()){
+        cout << "init_func " << name3 << " already exists." << endl;
+        abort();
+    }
+    init_func_C_map_[name3] = func_C3;
+
+    string name4 = "Largest Leftmost Spin Z Value";
+    init_func_C func_C4 = largest_leftmost_spin_z_complex_eigenstate;
+
+    it_C = init_func_C_map_.find(name4);
+    if (it_C != init_func_C_map_.end()){
+        cout << "init_func " << name4 << " already exists." << endl;
+        abort();
+    }
+    init_func_C_map_[name4] = func_C4;
+
+    string name5 = "Leftmost Spin Z Value";
+    init_func_C func_C5 = leftmost_spin_z_complex_eigenstate;
+
+    it_C = init_func_C_map_.find(name5);
+    if (it_C != init_func_C_map_.end()){
+        cout << "init_func " << name5 << " already exists." << endl;
+        abort();
+    }
+    init_func_C_map_[name5] = func_C5;
+
+    string name6 = "Full Leftmost Spin Z Value";
+    init_func_C func_C6 = leftmost_spin_z_complex_eigenstate;
+
+    it_C = init_func_C_map_.find(name6);
+    if (it_C != init_func_C_map_.end()){
+        cout << "init_func " << name6 << " already exists." << endl;
+        abort();
+    }
+    init_func_C_map_[name6] = func_C6;
+
+    string name7 = "Leftmost Spin Random State";
+    init_func_C func_C7 = leftmost_spin_random_state;
+
+    it_C = init_func_C_map_.find(name7);
+    if (it_C != init_func_C_map_.end()){
+        cout << "init_func " << name7 << " already exists." << endl;
+        abort();
+    }
+    init_func_C_map_[name7] = func_C7;
+
+}
+
+void InitObj::Multi_Num_Init(const string& init_name, InitInfo& init_info) const {
+    if (init_name == "Leftmost Spin Z Value"){
+        init_info.multi_ini_para_num = init_info.multi_ini_para.leftmost_spin_z_index_set.size();
+    }
+    else if (init_name == "Full Leftmost Spin Z Value"){
+        // Here the leftmost_spin_z_index_set is also initialized
+        init_info.multi_ini_para_num = init_info.dim;
+
+        init_info.multi_ini_para.leftmost_spin_z_index_set.resize(init_info.dim);
+        for (int i=0; i<init_info.dim; i++) init_info.multi_ini_para.leftmost_spin_z_index_set[i] = i+1;
+    }
+    else{
+        cout << init_name << " is not implemented with multiple sets of initial parameters."
+        << endl;
+        init_info.multi_ini_para_num = 0;
+    }
+}
+
+string InitObj::Init_Para_String(const string& init_name, const InitInfo& init_info) const {
+    stringstream output;
+    if (init_name == "Leftmost Spin Z Value"){
+        output << ",Leftmost_spin_z_index=" << init_info.leftmost_spin_z_index;
+        return output.str();
+    }
+    else return "";
+}
+
+
+string InitObj::Init_Para_String(const string& init_name, const AllPara& para) const {
+    stringstream output;
+    if (init_name == "Leftmost Spin Z Value"){
+        output << "Spin_z_index=" << para.evolution.leftmost_spin_z_index;
+        return output.str();
+    }
+    else return "";
+}
+
---
+++ init_obj.h
@@ -0,0 +1,112 @@
+//
+// Created by Liangsheng Zhang on 6/1/15.
+//
+
+#ifndef MBL_V1_INIT_OBJ_H
+#define MBL_V1_INIT_OBJ_H
+
+#include <map>
+#include <Eigen/Dense>
+#include "basis_transition.h"
+#include "parameters.h"
+
+using namespace std;
+using namespace Eigen;
+
+/**
+ ** This file includes classes and functions used for constructing initial states.
+ **/
+
+/*
+ * Information that is related to constructing initial states. Every initial state construction
+ * function just takes this structure, transition matrix class and the initial state vector.
+ */
+
+struct InitInfo
+{
+    int size; // System size
+    int dim; // Total dimension of Hilbert space
+    double norm_delta; // A small number used to check whether norm is 1
+    bool debug; // Whether output debug information
+
+    // A deep copy constructor except for multi_ini_para
+    InitInfo(const InitInfo&);
+
+    // Default constructor
+    InitInfo(){};
+};
+
+// Pointer to all possible initial state construction function which gives a state vector
+typedef void (*init_func)(const InitInfo&, const TransitionMatrix&, VectorXcd&);
+
+// Pointer to all possible initial state construction function which gives a density matrix
+typedef void (*init_func_C)(const InitInfo&, MatrixXcd&);
+
+class InitObj
+{
+private:
+    // Map that stores init_func and its name to be called
+    map<string, init_func>init_func_map_;
+
+    // Map that stores init_func_C and its name to be called
+    map<string, init_func_C>init_func_C_map_;
+
+    void map_init_(); // Initialize init_func_map and init_func_C_map
+
+public:
+    InitObj() {map_init_();}
+
+    // Use a string to access different initial construction functions for state vector
+    init_func Init_Func(const string&) const;
+
+    // Use a string to access different initial construction functions for density matrix
+    init_func_C Init_Func_C(const string&) const;
+
+    // Initialize multi_ini_para_num in InitInfo according to the initial condition string
+    void Multi_Num_Init(const string&, InitInfo&) const;
+
+    // Return possible strings for parameters of initial condition
+    string Init_Para_String(const string&, const InitInfo&) const;
+
+    // Return possible strings for parameters of initial condition
+    string Init_Para_String(const string&, const AllPara&) const;
+
+    // Print out all init_func
+    void Print() const;
+
+    // Print out all init_func_C
+    void Print_C() const;
+
+    ~InitObj(){};
+};
+
+/*
+ * Initial state functions
+ */
+
+void product_random(const InitInfo&, const TransitionMatrix&, VectorXcd&);
+
+void random_product(const InitInfo&, const TransitionMatrix&, VectorXcd&);
+void random_product(const InitInfo&, MatrixXcd&);
+
+void random_pure(const InitInfo&, MatrixXcd&);
+
+void largest_leftmost_spin_z_complex_eigenstate(const InitInfo&, MatrixXcd&);
+void leftmost_spin_z_complex_eigenstate(const InitInfo&, MatrixXcd&);
+
+void leftmost_spin_random_state(const InitInfo&, MatrixXcd&);
+
+
+/*
+ * Some useful functions for initial state construction
+ */
+// Construct a vector of amplitudes which can be used for random pure state
+void random_pure_amplitude(vector<complex<double> >&);
+// Check whehter the norm of a complex vector is close to 1
+void norm_check(const VectorXcd&, double, const string&);
+// Compute the complex density matrix of a corresponding normalized complex state vector
+void state_to_density(const VectorXcd&, MatrixXcd&);
+
+
+#endif //MBL_V1_INIT_OBJ_H
+
