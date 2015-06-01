bffa159dcd5d1b8608445e04dffd4cac0cced2bf
Liangsheng <liangshe@princeton.edu>
2015-06-01 12:01:57 -0400

    Implement evol_data for time evolution data collection using old codes

---
+++ evol_data.cpp
@@ -0,0 +1,231 @@
+//
+// Created by Liangsheng Zhang on 6/1/15.
+//
+
+#include "evol_data.h"
+
+using namespace std;
+
+/**
+ ** This file contains some common functions in evol_data.h
+ **/
+
+EvolData::EvolData(const AllPara& parameters): size_(parameters.generic.size){
+    func_status_ = parameters.evolution.evol_compute;
+    Data_Func_Map_Init_();
+
+    if ( func_status_.size() != data_init_.size()){
+        cout << "Number of registered functions in parameters for evolution is not the same as"
+        << " number of registered functions that can be called." << endl;
+        cout << "Functions in parameters:" << endl;
+        for (map<string, bool>::iterator it = func_status_.begin(); it != func_status_.end(); it++){
+            cout << it -> first << endl;
+        }
+        cout << "Total Number: " << func_status_.size() << endl;
+
+        cout << "Functions that can be called:" << endl;
+        for (map<string, Data_Init>::iterator it = data_init_.begin(); it != data_init_.end();
+             it ++){
+            cout << it -> first << endl;
+        }
+        cout << "Total Number:" << data_init_.size() << endl;
+
+        abort();
+    }
+
+    Name_Check_();
+
+    // Initialize all quantities which will be computed
+    for (map<string, bool>::iterator it = func_status_.begin(); it != func_status_.end(); it++){
+        if (it -> second) ( this ->* (data_init_[it -> first]) ) (parameters);
+    }
+}
+
+void EvolData::Print_All_Name() const{
+    map<string, bool>::const_iterator it;
+
+    for (it = func_status_.begin(); it != func_status_.end(); it ++){
+        cout << it -> first << endl;
+    }
+}
+
+void EvolData::Print_All_Status() const{
+    map<string, bool>::const_iterator it;
+
+    for (it = func_status_.begin(); it != func_status_.end(); it ++){
+        cout << it -> first << "   ";
+
+        if (it -> second) cout << "Would be computed." << endl;
+        else cout << "Would not be computed." << endl;
+    }
+}
+
+void EvolData::Name_Check_() const{
+    map<string, bool>::const_iterator para_it;
+    map<string, Data_Init>::const_iterator data_it;
+
+    for (para_it = func_status_.begin(); para_it != func_status_.end(); para_it ++){
+        data_it = data_init_.find(para_it -> first);
+        if (data_it == data_init_.end()){
+            cout << "Names in evolution are not consistent." << endl;
+
+            cout << "Names in parameters:" << endl;
+            map<string, bool>::const_iterator it;
+            for (it = func_status_.begin(); it!= func_status_.end(); it++){
+                cout << it -> first << endl;
+            }
+
+            cout << "Names in EvolData:" << endl;
+            for (data_it = data_init_.begin(); data_it != data_init_.end(); data_it ++){
+                cout << data_it -> first << endl;
+            }
+            abort();
+        }
+    }
+}
+
+void EvolData::Data_Compute(const VectorXcd& state, const StepInfo& info){
+    for (map<string, bool>::iterator it = func_status_.begin(); it != func_status_.end(); it++){
+        if (it -> second) ( this ->* (data_cal_[it -> first]) ) (state, info);
+    }
+}
+
+void EvolData::Data_Compute(const MatrixXcd& state_density, const StepInfo& info){
+    for (map<string, bool>::iterator it = func_status_.begin(); it != func_status_.end(); it++){
+        if (it -> second) ( this ->* (data_cal_C_[it -> first]) ) (state_density, info);
+    }
+}
+
+void EvolData::Data_Output(const AllPara& parameters, const string& type_name){
+    for (map<string, bool>::iterator it = func_status_.begin(); it != func_status_.end(); it++){
+        if (it -> second){
+            map<string, Data_Out>::iterator out_it = data_out_.find(it -> first);
+            if (out_it != data_out_.end())
+                ( this ->* (data_out_[it -> first]) ) (parameters, type_name);
+        }
+    }
+}
+
+void EvolData::Data_Func_Map_Init_(){
+    map<string, Data_Init>::iterator init_it;
+    map<string, Data_Cal>::iterator cal_it;
+    map<string, Data_Cal_C>::iterator cal_C_it;
+    map<string, Data_Out>::iterator out_it;
+
+    // Entropy Per Model data
+    string name1 = "Entropy Per Model";
+    Data_Init init_func1 = &EvolData::Entropy_Per_Model_Init_;
+    Data_Cal cal_func1 = &EvolData::Entropy_Per_Model_Cal_;
+    Data_Cal_C cal_C_func1 = &EvolData::Entropy_Per_Model_Cal_C_;
+    Data_Out out_func1 = &EvolData::Entropy_Per_Model_Out_;
+
+    // Make sure the name has not been used before
+    init_it = data_init_.find(name1);
+    cal_it = data_cal_.find(name1);
+    out_it = data_out_.find(name1);
+    if (init_it != data_init_.end() || cal_it != data_cal_.end() || out_it != data_out_.end()){
+        cout << name1 << " for evolution has appeared before." << endl;
+        abort();
+    }
+
+    data_init_[name1] = init_func1;
+    data_cal_[name1] = cal_func1;
+    data_cal_C_[name1] = cal_C_func1;
+    data_out_[name1] = out_func1;
+
+    // Leftmost Spin Z Per Model data
+    string name2 = "Leftmost Spin Z Per Model";
+    Data_Init init_func2 = &EvolData::Leftmost_Spin_Z_Per_Model_Init_;
+    Data_Cal cal_func2 = &EvolData::Leftmost_Spin_Z_Per_Model_Cal_;
+    Data_Cal_C cal_C_func2 = &EvolData::Leftmost_Spin_Z_Per_Model_Cal_C_;
+    Data_Out out_func2 = &EvolData::Leftmost_Spin_Z_Per_Model_Out_;
+
+    // Make sure the name has not been used before
+    init_it = data_init_.find(name2);
+    cal_it = data_cal_.find(name2);
+    out_it = data_out_.find(name2);
+    if (init_it != data_init_.end() || cal_it != data_cal_.end() || out_it != data_out_.end()){
+        cout << name2 << " for evolution has appeared before." << endl;
+        abort();
+    }
+
+    data_init_[name2] = init_func2;
+    data_cal_[name2] = cal_func2;
+    data_cal_C_[name2] = cal_C_func2;
+    data_out_[name2] = out_func2;
+
+    // Leftmost Spin X Per Model data
+    string name3 = "Leftmost Spin X Per Model";
+    Data_Init init_func3 = &EvolData::Leftmost_Spin_X_Per_Model_Init_;
+    Data_Cal cal_func3 = &EvolData::Leftmost_Spin_X_Per_Model_Cal_;
+    Data_Cal_C cal_C_func3 = &EvolData::Leftmost_Spin_X_Per_Model_Cal_C_;
+    Data_Out out_func3 = &EvolData::Leftmost_Spin_X_Per_Model_Out_;
+
+    // Make sure the name has not been used before
+    init_it = data_init_.find(name3);
+    cal_it = data_cal_.find(name3);
+    out_it = data_out_.find(name3);
+    if (init_it != data_init_.end() || cal_it != data_cal_.end() || out_it != data_out_.end()){
+        cout << name3 << " for evolution has appeared before." << endl;
+        abort();
+    }
+
+    data_init_[name3] = init_func3;
+    data_cal_[name3] = cal_func3;
+    data_cal_C_[name3] = cal_C_func3;
+    data_out_[name3] = out_func3;
+
+    // Leftmost Spin Y Per Model data
+    string name4 = "Leftmost Spin Y Per Model";
+    Data_Init init_func4 = &EvolData::Leftmost_Spin_Y_Per_Model_Init_;
+    Data_Cal cal_func4 = &EvolData::Leftmost_Spin_Y_Per_Model_Cal_;
+    Data_Cal_C cal_C_func4 = &EvolData::Leftmost_Spin_Y_Per_Model_Cal_C_;
+    Data_Out out_func4 = &EvolData::Leftmost_Spin_Y_Per_Model_Out_;
+
+    // Make sure the name has not been used before
+    init_it = data_init_.find(name4);
+    cal_it = data_cal_.find(name4);
+    out_it = data_out_.find(name4);
+    if (init_it != data_init_.end() || cal_it != data_cal_.end() || out_it != data_out_.end()){
+        cout << name4 << " for evolution has appeared before." << endl;
+        abort();
+    }
+
+    data_init_[name4] = init_func4;
+    data_cal_[name4] = cal_func4;
+    data_cal_C_[name4] = cal_C_func4;
+    data_out_[name4] = out_func4;
+
+    // Check data_init_ and data_cal_ have the same size
+    if (data_init_.size() != data_cal_.size()){
+        cout << "Number of initializations in evolution is not the same as number of calculations."
+        << endl;
+        cout << "Registered initializations:" << endl;
+        for (init_it = data_init_.begin(); init_it != data_init_.end(); init_it ++){
+            cout << init_it -> first << endl;
+        }
+        cout << "Total Number: " << data_init_.size();
+
+        cout << "Registered calculations:" << endl;
+        for (cal_it = data_cal_.begin(); cal_it != data_cal_.end(); cal_it ++){
+            cout << cal_it -> first << endl;
+        }
+        cout << "Total Number: " << data_cal_.size();
+    }
+
+    // Check data_init_ and data_out_ have the same size
+    if (data_init_.size() != data_out_.size() ){
+        cout << "Number of initializations in evolution is not the same as number of output."
+        << endl;
+        cout << "Registered initializations:" << endl;
+        for (init_it = data_init_.begin(); init_it != data_init_.end(); init_it ++){
+            cout << init_it -> first << endl;
+        }
+        cout << "Total Number: " << data_init_.size();
+
+        cout << "Registered output for each model:" << endl;
+        for (out_it = data_out_.begin(); out_it != data_out_.end(); out_it ++){
+            cout << out_it -> first << endl;
+        }
+    }
+}
---
+++ evol_data.h
@@ -0,0 +1,135 @@
+//
+// Created by Liangsheng Zhang on 6/1/15.
+//
+
+#ifndef MBL_V1_EVOL_DATA_H
+#define MBL_V1_EVOL_DATA_H
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <Eigen/Dense>
+#include "parameters.h"
+
+using namespace std;
+using namespace Eigen;
+
+/**
+ ** This file includes the class for collecting and outputing data in evolution
+ **/
+
+/*
+ * This structure gives relevant information at each step that can be used for computation.
+ */
+struct StepInfo{
+    int model; // The index of current model
+    int realization; // The index of current realization
+    int time; // The index of current time step
+    int init_condition; // The index of given initial condition
+    bool debug; // Whether output debug information
+
+    double delta; // A small number
+
+    int left_size; // If partition the chain to two halves, the size of left part
+};
+
+class EvolData;
+
+// Functions that initialize data using parameters passed in
+typedef void (EvolData::*Data_Init)(const AllPara&);
+
+// Functions that compute data from a state vector and step information
+typedef void (EvolData::*Data_Cal)(const VectorXcd&, const StepInfo&);
+
+// Functions that compute data from a complex density matrix and step information
+typedef void (EvolData::*Data_Cal_C)(const MatrixXcd&, const StepInfo&);
+
+// Functions that output data for each model using parameters. The string is used as part of
+// the output file name
+typedef void (EvolData::*Data_Out)(const AllPara&, const string&);
+
+class EvolData
+{
+private:
+    map<string,bool> func_status_; // Determine whether a particular data type is calculated
+
+
+    map<string, Data_Init> data_init_; // Correlate name with data_init function
+    map<string, Data_Cal> data_cal_; // Correlate name with data_cal function
+    map<string, Data_Cal_C> data_cal_C_; // Correlate name with data_cal_C function
+    map<string, Data_Out> data_out_; // Correlate name with data_out function
+
+    void Data_Func_Map_Init_(); // Initialize data_init_ and data_cal_;
+    void Name_Check_() const; // Check names in different maps are consistent
+
+    // Entropy per model. The outer index is for time; the inner index is for realization
+    vector<vector<double> > entropy_per_model_;
+    void Entropy_Per_Model_Init_(const AllPara&); // Initialize entropy_per_model
+    // Compute entropy_per_model given state vector
+    void Entropy_Per_Model_Cal_(const VectorXcd&, const StepInfo&);
+    // Compute entropy_per_model given complex density matrix
+    void Entropy_Per_Model_Cal_C_(const MatrixXcd&, const StepInfo&);
+    // Output entropy_per_model
+    void Entropy_Per_Model_Out_(const AllPara&, const string&);
+
+    // Average leftmost spin z per model. The outer index is for time; the inner index is for
+    // realization
+    vector<vector<double> > leftmost_spin_z_per_model_;
+    // Initialize leftmost_spin_z_per_model
+    void Leftmost_Spin_Z_Per_Model_Init_(const AllPara&);
+    // Compute leftmost_spin_z_per_model given state vector, not implemented yet
+    void Leftmost_Spin_Z_Per_Model_Cal_(const VectorXcd&, const StepInfo&);
+    // Compute leftmost_spin_z_per_model given complex density matrix
+    void Leftmost_Spin_Z_Per_Model_Cal_C_(const MatrixXcd&, const StepInfo&);
+    // Output leftmost_spin_z_per_model
+    void Leftmost_Spin_Z_Per_Model_Out_(const AllPara&, const string&);
+
+    // Average leftmost spin x per model. The outer index is for time; the inner index is for
+    // realization
+    vector<vector<double> > leftmost_spin_x_per_model_;
+    // Initialize leftmost_spin_x_per_model
+    void Leftmost_Spin_X_Per_Model_Init_(const AllPara&);
+    // Compute leftmost_spin_x_per_model given state vector, not implemented yet
+    void Leftmost_Spin_X_Per_Model_Cal_(const VectorXcd&, const StepInfo&);
+    // Compute leftmost_spin_x_per_model given complex density matrix
+    void Leftmost_Spin_X_Per_Model_Cal_C_(const MatrixXcd&, const StepInfo&);
+    // Output leftmost_spin_x_per_model
+    void Leftmost_Spin_X_Per_Model_Out_(const AllPara&, const string&);
+
+    // Average leftmost spin y per model. The outer index is for time; the inner index is for
+    // realization
+    vector<vector<double> > leftmost_spin_y_per_model_;
+    // Initialize leftmost_spin_y_per_model
+    void Leftmost_Spin_Y_Per_Model_Init_(const AllPara&);
+    // Compute leftmost_spin_y_per_model given state vector, not implemented yet
+    void Leftmost_Spin_Y_Per_Model_Cal_(const VectorXcd&, const StepInfo&);
+    // Compute leftmost_spin_y_per_model given complex density matrix
+    void Leftmost_Spin_Y_Per_Model_Cal_C_(const MatrixXcd&, const StepInfo&);
+    // Output leftmost_spin_y_per_model
+    void Leftmost_Spin_Y_Per_Model_Out_(const AllPara&, const string&);
+
+
+    const int size_; // Size of the system
+
+public:
+    EvolData(const AllPara&);
+
+    void Print_All_Name() const; // Print all possible names of data type calculation
+    void Print_All_Status() const; // Print all possible data type calculation, indicating
+    // whether they will be calculated
+
+    // Compute data at each step given a state vector. The entry which is true in
+    // func_status will be computed.
+    void Data_Compute(const VectorXcd&, const StepInfo&);
+
+    // Compute data at each step given a complex density matrix. The entry which is true in
+    // func_status will be computed.
+    void Data_Compute(const MatrixXcd&, const StepInfo&);
+
+    // Output data to file. All the data that are computed will be outputted
+    void Data_Output(const AllPara&, const string&);
+};
+
+
+#endif //MBL_V1_EVOL_DATA_H
