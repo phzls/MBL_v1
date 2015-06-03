d81883a0189e0a888868d11be0f583349ee37e57
Liangsheng <liangshe@princeton.edu>
2015-06-03 10:47:24 -0400

    Obtain initial state names in task_model script

--- task_model.py
+++ task_model.py
@@ -6,19 +6,21 @@ import file_process as fp
 
 class TASK_MODEL(object):
     """
-    Record tasks and models of the program
+    Record tasks, models and initial conditions of the program
     """
     def __init__(self):
         self.tasks = []
         self.models = []
+        self.inits = []
 
 
 def task_model(tasks_models):
     """
     Build and run task_model program, and record the outputted tasks and models. Here
-    it is assumed that in the output only two lines have keywords "tasks" or "models",
-    and below two lines the names of tasks or models are given as the second word.
-    :param tasks_models: The data structure that holds tasks and models
+    it is assumed that in the output only three lines have keywords "tasks", "models"
+    or "Initial State Construction Functions", and below these lines the names of tasks,
+    models and init_func are given as the second word.
+    :param tasks_models: The data structure that holds tasks, models and init_func.
     :return: None
     """
     make_process = subprocess.Popen("make task_model -j4",stderr=subprocess.STDOUT, shell=True)
@@ -30,6 +32,7 @@ def task_model(tasks_models):
 
     find_model = False
     find_task = False
+    find_init = False
 
     while True:
         line = proc.stdout.readline()
@@ -41,10 +44,21 @@ def task_model(tasks_models):
                 # The line with keyword "models"
                 find_model = True
                 find_task = False
+                find_init = False
             elif line.find("tasks") > -1:
                 # The line with keyword "tasks"
                 find_model = False
                 find_task = True
+                find_init = False
+            elif line.find("Initial State Construction Functions") > -1:
+                # The line with keyword "Initial State Construction Functions"
+                find_model = False
+                find_task = False
+                find_init = True
+            elif line.find("Initial Density Construction Functions") > -1:
+                find_model = False
+                find_task = False
+                find_init = False
             elif len(line) > 1:
                 # Not empty line
                 find_content = True
@@ -56,5 +70,7 @@ def task_model(tasks_models):
                     tasks_models.models.append(content[1])
                 elif find_task:
                     tasks_models.tasks.append(content[1])
+                elif find_init:
+                    tasks_models.inits.append(content[0])
         else:
             break
