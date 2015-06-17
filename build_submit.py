ffaf27cbfbea87f754b0476c4a9c04e793c6ba49
Liangsheng <liangshe@princeton.edu>
2015-06-17 17:16:24 -0400

    Rewrite build_submit, evol_J_run_build_submit and size_run_build_submit using core

--- build_submit.py
+++ build_submit.py
@@ -3,7 +3,7 @@ __author__ = 'liangshengzhang'
 import subprocess
 import task_model as tm
 import file_process as fp
-import submit as sb
+import core as core
 import os
 import fnmatch
 
@@ -97,100 +97,11 @@ try:
                             + " ./parameters/" + key + new_match_name, shell=True )
             file_modify[key] = True
 
-    if system == "Darwin\n":
-        # It is a mac system
-        cont = raw_input("Continue? ")
-        if (cont == '') or cont.startswith('y') or cont.startswith('Y'):
-            make_process = subprocess.Popen("make auto OUT=mbl_auto -j4",stderr=subprocess.STDOUT, shell=True)
-            if make_process.wait() != 0:
-                fp.file_clean(count, file_modify)
-                raise Exception("Make Error")
+    # Handle the executable
+    temp_success_run = core.core(system=system, count=count, file_modify=file_modify,
+                                 exe_folder=exe_folder, data=data)
+    success_run = success_run and temp_success_run
 
-            run = subprocess.Popen("./mbl_auto " + str(count), stdout=subprocess.PIPE, shell=True)
-            stdout = []
-            while True:
-                line = run.stdout.readline()
-                stdout.append(line)
-                print line,
-                if line == '' and run.poll() != None:
-                    break
-            success_run = True
-
-    elif system == "Linux\n":
-        # Make the program
-        progname = data.model.lower() + "_" + data.size + data.model_para + "_" \
-                   + data.task.lower() + data.task_para
-        make_process = subprocess.Popen("make auto OUT=" + progname
-                                        + " -j4",stderr=subprocess.STDOUT, shell=True)
-        if make_process.wait() != 0:
-            fp.file_clean(count, file_modify)
-            raise Exception("Make Error")
-
-        # Move the executable to the folder
-        subprocess.call("mv " + progname + "./" + exe_folder, stdout=subprocess.PIPE, shell=True)
-
-        valid_choice = False
-        while valid_choice is False:
-            choice = raw_input("Submit (s), Direct Run (r), Valgrind (v) or Exit (e)?\n")
-
-            if choice.startswith("r") or choice.startswith("R"):
-                run = subprocess.Popen("./" + exe_folder+ '/' + progname + " " + str(count),
-                                       stdout=subprocess.PIPE, shell=True)
-                stdout = []
-                while True:
-                    line = run.stdout.readline()
-                    stdout.append(line)
-                    print line,
-                    if line == '' and run.poll() != None:
-                        break
-                valid_choice = True
-                success_run = True
-
-            elif choice.startswith("v") or choice.startswith("V"):
-                if int(data.num_threads) > 1:
-                    print "Valgrind has problems handling multi-thread."
-                else:
-                    val = subprocess.Popen("valgrind --leak-check=full ./" + exe_folder + '/' + progname
-                                           + " " + str(count), stdout=subprocess.PIPE, shell=True)
-                    stdout = []
-                    while True:
-                        line = val.stdout.readline()
-                        stdout.append(line)
-                        print line
-                        if line == '' and val.poll() != None:
-                            break
-                        valid_choice = True
-                        success_run = True
-
-            elif choice.startswith("e") or choice.startswith("E") or choice.startswith('q') or choice.startswith('Q'):
-                # Exit
-                valid_choice = True
-
-            elif choice.startswith("s") or choice.startswith("S"):
-
-                # Determine server type
-                f = open("submit_template.run",'r')
-                for line in f:
-                    if line.startswith("#Server"):
-                        server = line.split()[1]
-                        break
-                    elif line.startswith("# Server"):
-                        server = line.split()[2]
-                        break
-
-                if (server == "Adroit") or (server == "adroit"):
-                    sb.adroit_submit(data, progname, count, exe_folder)
-
-                elif (server == "Feynman") or (server == "feynman"):
-                    sb.feynman_submit(data, progname, count, exe_folder)
-
-                valid_choice = True
-                success_run = True
-
-            else:
-                print fp.bcolors.FAIL + "Invalid choice." + fp.bcolors.ENDC
-    else:
-        print fp.bcolors.FAIL + "Unknown system type." + fp.bcolors.ENDC
 except Exception:
     print "An error occurred."
     fp.file_clean(count, file_modify)
--- evol_J_run_build_submit.py
+++ evol_J_run_build_submit.py
@@ -3,12 +3,13 @@ __author__ = 'liangshengzhang'
 import subprocess
 import task_model as tm
 import file_process as fp
-import submit as sb
+import core as core
 import os
 import fnmatch
 
 data = fp.DATA()
 tasks_models = tm.TASK_MODEL()
+exe_folder = "executables" # The folder name for executables
 
 modify_words = ["size", "num_realizations", "J", "init_func_name", "time_step", "log_time"]
 
@@ -20,6 +21,10 @@ system = proc.communicate()[0]
 
 if system == "Linux\n":
     subprocess.call("module load intel", stdout=subprocess.PIPE, shell=True)
+
+    # Construct a folder for executables if it does not exist
+    if os.path.isdir("./" + exe_folder) is False:
+        subprocess.call("mkdir " + exe_folder, stdout=subprocess.PIPE, shell=True)
 
 # Obtain count and update count file
 count = None
@@ -37,134 +42,54 @@ for file in os.listdir('./parameters'):
         start = file.find(match_name)
         file_modify[file[:start]] = False
 
+# In case any exception is thrown, the program can end gracefully
+try:
+    # Obtain tasks and models
+    tm.task_model(tasks_models)
 
-# Obtain tasks and models
-tm.task_model(tasks_models)
+    # Generate parameters
+    fp.para_gen("generic_para", tasks_models, data, count, modify_words)
+    file_modify["generic_para"] = True
 
-# Generate parameters
-fp.para_gen("generic_para", tasks_models, data, count, modify_words)
-file_modify["generic_para"] = True
+    # Generate Model data
+    fp.para_gen(data.model.lower(), tasks_models, data, count, modify_words)
+    file_modify[data.model.lower()] = True
 
-# Generate Model data
-fp.para_gen(data.model.lower(), tasks_models, data, count, modify_words)
-file_modify[data.model.lower()] = True
+    # Generate Task data
+    fp.para_gen(data.task.lower(), tasks_models, data, count, modify_words)
+    file_modify[data.task.lower()] = True
 
-# Generate Task data
-fp.para_gen(data.task.lower(), tasks_models, data, count, modify_words)
-file_modify[data.task.lower()] = True
+    print fp.bcolors.BOLD, "System size is " + str(data.size) + "\n", fp.bcolors.ENDC
 
-print fp.bcolors.BOLD, "System size is " + str(data.size) + "\n", fp.bcolors.ENDC
+    # Update all files which are not modified to have correct file names
+    new_match_name = "_" + str(count) + ".dat"
+    for key in file_modify:
+        if file_modify[key] is False:
+            subprocess.call("cp ./parameters/" + key + match_name
+                            + " ./parameters/" + key + new_match_name, shell=True )
+            file_modify[key] = True
 
-# Update all files which are not modified to have correct file names
-new_match_name = "_" + str(count) + ".dat"
-for key in file_modify:
-    if file_modify[key] is False:
-        subprocess.call("cp ./parameters/" + key + match_name
-                        + " ./parameters/" + key + new_match_name, shell=True )
-        file_modify[key] = True
+    # Ask whether user want running details
+    run_detail = None
+    while run_detail is None:
+        detail = raw_input("Show running details?")
+        if (detail == '') or detail.lower().startswith('n'):
+            run_detail = False
+        elif detail.lower().startswith('y'):
+            run_detail = True
+            fp.display_file("generic_para", data, count)
+            fp.display_file(data.model.lower(), data, count)
+            fp.display_file(data.task.lower(), data, count)
 
-# Ask whether user want running details
-run_detail = None
-while run_detail is None:
-    detail = raw_input("Show running details?")
-    if (detail == '') or detail.lower().startswith('n'):
-        run_detail = False
-    elif detail.lower().startswith('y'):
-        run_detail = True
-        fp.display_file("generic_para", data, count)
-        fp.display_file(data.model.lower(), data, count)
-        fp.display_file(data.task.lower(), data, count)
+    # Handle the executable
+    temp_success_run = core.core(system=system, count=count, file_modify=file_modify,
+                                 exe_folder=exe_folder, data=data)
+    success_run = success_run and temp_success_run
 
-if system == "Darwin\n":
-    # It is a mac system
-    cont = raw_input("Continue? ")
-    if (cont == '') or cont.startswith('y') or cont.startswith('Y'):
-        make_process = subprocess.Popen("make auto OUT=mbl_auto -j4",stderr=subprocess.STDOUT, shell=True)
-        if make_process.wait() != 0:
-            fp.file_clean(count, file_modify)
-            raise Exception("Make Error")
-
-        run = subprocess.Popen("./mbl_auto " + str(count), stdout=subprocess.PIPE, shell=True)
-        stdout = []
-        while True:
-            line = run.stdout.readline()
-            stdout.append(line)
-            print line,
-            if line == '' and run.poll() != None:
-                break
-        success_run = True
-
-elif system == "Linux\n":
-    # Make the program
-    progname = data.model.lower() + "_" + data.size + data.model_para + "_" \
-               + data.task.lower() + data.task_para
-    make_process = subprocess.Popen("make auto OUT=" + progname
-                                    + " -j4",stderr=subprocess.STDOUT, shell=True)
-    if make_process.wait() != 0:
-        fp.file_clean(count, file_modify)
-        raise Exception("Make Error")
-
-    valid_choice = False
-    while valid_choice is False:
-        choice = raw_input("Submit (s), Direct Run (r), Valgrind (v) or Exit (e)?\n")
-
-        if choice.startswith("r") or choice.startswith("R"):
-            run = subprocess.Popen("./" + progname + " " + str(count), stdout=subprocess.PIPE, shell=True)
-            stdout = []
-            while True:
-                line = run.stdout.readline()
-                stdout.append(line)
-                print line,
-                if line == '' and run.poll() != None:
-                    break
-            valid_choice = True
-            success_run = True
-
-        elif choice.startswith("v") or choice.startswith("R"):
-            if int(data.num_threads) > 1:
-                print "Valgrind has problems handling multi-thread."
-            else:
-                val = subprocess.Popen("valgrind --leak-check=full ./" + progname + " " + str(count),
-                                       stdout=subprocess.PIPE, shell=True)
-                stdout = []
-            while True:
-                line = val.stdout.readline()
-                stdout.append(line)
-                print line,
-                if line == '' and val.poll() != None:
-                    break
-                valid_choice = True
-                success_run = True
-
-        elif choice.startswith("e") or choice.startswith("E") or choice.startswith('q') or choice.startswith('Q'):
-            # Exit
-            valid_choice = True
-
-        elif choice.startswith("s") or choice.startswith("S"):
-
-            # Determine server type
-            f = open("submit_template.run",'r')
-            for line in f:
-                if line.startswith("#Server"):
-                    server = line.split()[1]
-                    break
-                elif line.startswith("# Server"):
-                    server = line.split()[2]
-                    break
-
-            if (server == "Adroit") or (server == "adroit"):
-                sb.adroit_submit(data, progname, count)
-
-            elif (server == "Feynman") or (server == "feynman"):
-                sb.feynman_submit(data, progname, count)
-
-            valid_choice = True
-            success_run = True
-
-        else:
-            print fp.bcolors.FAIL + "Invalid choice." + fp.bcolors.ENDC
-else:
-    print fp.bcolors.FAIL + "Unknown system type." + fp.bcolors.ENDC
+except Exception:
+    print "An error occurred."
+    fp.file_clean(count, file_modify)
+    raise
 
 if success_run:
     file = "./parameters/count.txt"
