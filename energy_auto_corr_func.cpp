4de74c10089c91972df2e2da8ddd3bcfaf086f25
Liangsheng <liangshe@princeton.edu>
2015-09-27 14:56:38 -0400

    Implement energy_auto_corr_func which computes the energy operator

---
+++ energy_auto_corr_func.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Liangsheng Zhang on 9/25/15.
+//
+
+/*
+ * This file implements some functions related to energy_auto_corr
+ */
+
+#include "energy_auto_corr_func.h"
+
+using namespace std;
+using namespace Eigen;
+
+void Ham_for_Ising_Random_Simp_Shift_Cos_Real_Flo(const EvolOP* floquet, MatrixXcd& ham){
+    floquet -> Get_Ham(ham, "Eigen");
+}
+
---
+++ energy_auto_corr_func.h
@@ -0,0 +1,13 @@
+//
+// Created by Liangsheng Zhang on 9/25/15.
+//
+
+#ifndef MBL_V1_ENERGY_AUTO_CORR_FUNC_H
+#define MBL_V1_ENERGY_AUTO_CORR_FUNC_H
+
+#include <Eigen/Dense>
+#include "evol_op.h"
+
+void Ham_for_Ising_Random_Simp_Shift_Cos_Real_Flo(const EvolOP*, Eigen::MatrixXcd&);
+
+#endif //MBL_V1_ENERGY_AUTO_CORR_FUNC_H
