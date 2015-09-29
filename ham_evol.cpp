d3bb6d9aee1101dadaaf95771bc76a0367a67b4a
Liangsheng <liangshe@princeton.edu>
2015-09-29 13:22:51 -0400

    Implement Transition_Compute in ham_evol

--- ham_evol.cpp
+++ ham_evol.cpp
@@ -28,3 +28,44 @@ void HamEvolVanillaReal::Eval(vector<VectorXd>& eval) const {
     eval.resize(1);
     eval[0] = eigen_ -> eigenvalues();
 }
+
+void HamEvolVanillaReal::Transition_Compute(TransitionMatrix& transition, const string& matrix_name) const{
+    if (matrix_name == "Basic_Full"){
+        if (eigen_info_) transition.Basic_Full(eigen_ -> eigenvectors());
+        else{
+            cout << "Evolution Operator " << Repr() <<" has not been diagonalized." << endl;
+            abort();
+        }
+    }
+    else{
+        cout << "Transition matrix " << matrix_name << " cannot be established for "
+        << Type() << endl;
+        abort();
+    }
+}
+
+void HamEvolVanillaReal::Transition_Compute(TransitionMatrix& transition, const string& matrix_name,
+                                            const vector<MatrixXcd>& evec) const{
+    if (matrix_name == "Basic_Full"){
+        if (eigen_info_) transition.Basic_Full(eigen_ -> eigenvectors());
+        else transition.Basic_Full(evec[0]);
+    }
+    else{
+        cout << "Transition matrix " << matrix_name << " cannot be established for "
+        << Type() << endl;
+        abort();
+    }
+}
+
+void HamEvolVanillaReal::Transition_Compute(TransitionMatrix& transition, const string& matrix_name,
+                                            const vector<MatrixXd>& evec) const{
+    if (matrix_name == "Basic_Full"){
+        if (eigen_info_) transition.Basic_Full(eigen_ -> eigenvectors());
+        else transition.Basic_Full(evec[0]);
+    }
+    else{
+        cout << "Transition matrix " << matrix_name << " cannot be established for "
+        << Type() << endl;
+        abort();
+    }
+}
