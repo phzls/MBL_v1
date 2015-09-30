b1139f45d35b9dc8af1faa002a19ef032fa74b3a
Liangsheng <liangshe@princeton.edu>
2015-09-30 17:52:59 -0400

    Add Ham_for_Heisen_Random_Cos_Sz_Sector_Modified_Flo in energy_auto_corr_func

--- energy_auto_corr_func.h
+++ energy_auto_corr_func.h
@@ -10,8 +10,10 @@
 
 void Ham_for_Ising_Random_Simp_Shift_Cos_Real_Flo(const EvolOP*, Eigen::MatrixXcd&);
 
-void Ham_for_Heisen_Random_Cos_Sz_Sector_Shift_Real_Flo(const EvolOP* floquet, MatrixXcd&);
+void Ham_for_Heisen_Random_Cos_Sz_Sector_Shift_Real_Flo(const EvolOP*, MatrixXcd&);
 
-void Ham_for_Heisen_Quasi_Sz_Sector_Shift_Real_Flo(const EvolOP* floquet, MatrixXcd&);
+void Ham_for_Heisen_Quasi_Sz_Sector_Shift_Real_Flo(const EvolOP*, MatrixXcd&);
+
+void Ham_for_Heisen_Random_Cos_Sz_Sector_Modified_Flo(const EvolOP*, MatrixXcd&);
 
 #endif //MBL_V1_ENERGY_AUTO_CORR_FUNC_H
