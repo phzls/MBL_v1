f2c2323e91eb31e027805f7c8d81b7e449312fba
Liangsheng <liangshe@princeton.edu>
2015-09-30 16:55:56 -0400

    Add models Heisen_Random_Cos_Sz_Sector_Modified_Tau_Flo and Heisen_Quasi_Sz_Sector_Modified_Tau_Flo in main_auto

--- main_auto.cpp
+++ main_auto.cpp
@@ -50,6 +50,8 @@ int main(int argc, char* argv[]){
     model_para_read_map["Heisen_Quasi_Sz_Sector_Shift_Real_Tau_Flo"] = Heisen_Quasi_Sz_Sector_Shift_Real_Tau_Flo_Para;
     model_para_read_map["Ising_Random_Simp_Cos_Ham"] = Ising_Random_Simp_Cos_Ham_Para;
     model_para_read_map["Ising_Quasi_Simp_Ham"] = Ising_Quasi_Simp_Ham_Para;
+    model_para_read_map["Heisen_Random_Cos_Sz_Sector_Modified_Tau_Flo"] = Heisen_Random_Cos_Sz_Sector_Modified_Tau_Flo_Para;
+    model_para_read_map["Heisen_Quasi_Sz_Sector_Modified_Tau_Flo"] = Heisen_Quasi_Sz_Sector_Modified_Tau_Flo_Para;
 
     model_para_read_map["Heisen_Random_Cos_Sz_Sector_Ham"] = Heisen_Random_Cos_Sz_Sector_Ham_Para;
     model_para_read_map["Heisen_Quasi_Sz_Sector_Ham"] = Heisen_Quasi_Sz_Sector_Ham_Para;
