ee3accc1641aabf4277b2476389158c332c13c1d
Liangsheng <liangshe@princeton.edu>
2015-10-05 17:58:07 -0400

    Add modified heisenberg hamiltonian for model map in main_auto

--- main_auto.cpp
+++ main_auto.cpp
@@ -48,13 +48,15 @@ int main(int argc, char* argv[]){
     model_para_read_map["Ising_Random_Simp_Shift_Cos_Real_Tau_Flo"] = Ising_Random_Simp_Shift_Cos_Real_Tau_Flo_Para;
     model_para_read_map["Heisen_Random_Cos_Sz_Sector_Shift_Real_Tau_Flo"] = Heisen_Random_Cos_Sz_Sector_Shift_Real_Tau_Flo_Para;
     model_para_read_map["Heisen_Quasi_Sz_Sector_Shift_Real_Tau_Flo"] = Heisen_Quasi_Sz_Sector_Shift_Real_Tau_Flo_Para;
-    model_para_read_map["Ising_Random_Simp_Cos_Ham"] = Ising_Random_Simp_Cos_Ham_Para;
-    model_para_read_map["Ising_Quasi_Simp_Ham"] = Ising_Quasi_Simp_Ham_Para;
     model_para_read_map["Heisen_Random_Cos_Sz_Sector_Modified_Tau_Flo"] = Heisen_Random_Cos_Sz_Sector_Modified_Tau_Flo_Para;
     model_para_read_map["Heisen_Quasi_Sz_Sector_Modified_Tau_Flo"] = Heisen_Quasi_Sz_Sector_Modified_Tau_Flo_Para;
 
     model_para_read_map["Heisen_Random_Cos_Sz_Sector_Ham"] = Heisen_Random_Cos_Sz_Sector_Ham_Para;
     model_para_read_map["Heisen_Quasi_Sz_Sector_Ham"] = Heisen_Quasi_Sz_Sector_Ham_Para;
+    model_para_read_map["Ising_Random_Simp_Cos_Ham"] = Ising_Random_Simp_Cos_Ham_Para;
+    model_para_read_map["Ising_Quasi_Simp_Ham"] = Ising_Quasi_Simp_Ham_Para;
+    model_para_read_map["Heisen_Modified_Random_Cos_Sz_Sector_Ham"] = Heisen_Modified_Random_Cos_Sz_Sector_Ham_Para;
+    model_para_read_map["Heisen_Modified_Quasi_Sz_Sector_Ham"] = Heisen_Modified_Quasi_Sz_Sector_Ham_Para;
 
     task_para_read_map["Disorder_Transition"] = Disorder_Transition_Para;
     task_para_read_map["Single_Model_Time_Evolution"] = Single_Model_Time_Evolution_Para;
