74c1d2de745600d8da2df49ae1e8a129c5df6e4e
Liangsheng <liangshe@princeton.edu>
2016-01-27 20:36:42 -0500

    Add entropy calculation and model parameter generation functions in methods

--- methods.h
+++ methods.h
@@ -6,7 +6,7 @@
 #define MBL_V1_METHODS_H
 
 /*
- * This file contains methods which are used in evolution/level statistics.
+ * This file contains methods which are used in various main tasks.
  */
 
 #include <utility>
@@ -35,5 +35,27 @@ void evec_to_basic(const EvolOP*, const vector<MatrixXcd>&, vector<vector<comple
 void reduced_density_left_2(const VectorXcd&, int, int, MatrixXcd&);
 void reduced_density_left_2(const VectorXd&, int, int, MatrixXcd&);
 void reduced_density_left_2(const VectorXd&, int, int, MatrixXd&);
+
+/*
+ * Construct a vector of left-side binary entropies for a given model of spin chain where
+ * local dimension is 2. The integer is the size of the left part.
+ */
+void model_entropy_left_2(const EvolOP*, int, vector<double>&);
+
+/*
+ * Construct the left entropy of a given vector state from a model of spin chain where
+ * local dimension is 2. The first integer is the total size of the spin chain, and
+ * the second is the size of left part.
+ */
+double state_entropy_left_2(const VectorXd&, int, int);
+double state_entropy_left_2(const VectorXcd&,int, int);
+
+/*
+ * Generating random parameters used for various models according to the model name
+ * and possible configuration parameters. The number of parameters generated for
+ * each inner vector is given by the integer
+ */
+void model_para_generation(const AllPara&, vector< vector<double> >&, int);
+
 
 #endif //MBL_V1_METHODS_H
