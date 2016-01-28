6b2a80764177e94fc2243d17dfb68736931ccba6
Liangsheng <liangshe@princeton.edu>
2016-01-27 22:05:38 -0500

    Read order for linked cluster in para_model_task

--- para_model_task.cpp
+++ para_model_task.cpp
@@ -564,10 +564,13 @@ void Linked_Cluster_Para(AllPara& parameters, string count){
     keyword = "J_min";
     para_get(filename, content, keyword, parameters.floquet.J_min);
 
+    keyword = "order";
+    para_get(filename, content, keyword, parameters.linked_cluster_para.order);
+
     vector<string> linked_cluster_map;
     vector<string>::iterator it;
 
-    linked_cluster_map.push_back("Average Entropy");
+    linked_cluster_map.push_back("Average_Entropy");
 
     for(it = linked_cluster_map.begin(); it != linked_cluster_map.end(); it++){
         bool choice;
