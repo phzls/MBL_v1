32e6bd66750611b8a9192770baf12d3e90e0f182
Liangsheng <liangshe@princeton.edu>
2016-01-27 22:05:48 -0500

    Update main

--- main.cpp
+++ main.cpp
@@ -166,7 +166,7 @@ int main() {
 
     // Methods to be called for studying coefficients of linked clusters
     // Average entropy among all eigenstates
-    parameters.linked_cluster_para.linked_cluster_cal["Average Entropy"] = true;
+    parameters.linked_cluster_para.linked_cluster_cal["Average_Entropy"] = true;
 
 
 
